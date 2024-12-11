503c573d3656bb9ad428c019bf3e35377de6a33c
Toluener <63416512+Toluener@users.noreply.github.com>
2024-12-11 22:20:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-  printf("Hello, World!\n");
-  return 0;
+int feet, fathoms;
+ fathoms = 2;
+ feet = 6 * fathoms;
+ printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+ printf("Yes, I said %d feet!\n", 6 * fathoms);
+ return 0;
 }
